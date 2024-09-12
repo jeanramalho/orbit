cd270a6733951d65b3596c232f9481ad867b60e4
Jean Ramalho <jeanramalho@MacBook-Pro-de-MacBook.local>
2024-09-12 16:56:59 -0300

    Termina ajustes de estrutura e comeca a criar funcionalidades do backend

--- backend/src/db/seeds.ts
+++ backend/src/db/seeds.ts
@@ -1,15 +1,28 @@
-import { db } from '.'
+import { client, db } from '.'
 import { goalCompletions, goals } from './schema'
+import dayjs from 'dayjs'
 
 async function seed() {
   await db.delete(goalCompletions)
   await db.delete(goals)
 
-  await db.insert(goals).values([
-    { title: 'acordar cedo', desiredWeeklyFrequency: 5 },
-    { title: 'exercicio', desiredWeeklyFrequency: 3 },
-    { title: 'meditar', desiredWeeklyFrequency: 2 },
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: 'acordar cedo', desiredWeeklyFrequency: 5 },
+      { title: 'exercicio', desiredWeeklyFrequency: 3 },
+      { title: 'meditar', desiredWeeklyFrequency: 2 },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.insert(goalCompletions).values([
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
   ])
 }
 
-seed()
+seed().finally(() => {
+  client.end()
+})
