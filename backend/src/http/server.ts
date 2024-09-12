7a166d83345cbd626c3e6b646bd81b566a01913d
Jean Ramalho <jeanramalho@MacBook-Pro-de-MacBook.local>
2024-09-12 18:37:20 -0300

    Cria funcoes de queries para rotas

--- backend/src/http/server.ts
+++ backend/src/http/server.ts
@@ -1,10 +1,33 @@
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/create-goal'
+import { createCompletionRoute } from './routes/create-completion'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
 
-import fastify from "fastify";
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-const app = fastify()
-
-app.listen({
-    port: 3333,
-}).then(() => {
-    console.log("HTTP server running")
+app.register(fastifyCors, {
+  origin: '*',
 })
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
